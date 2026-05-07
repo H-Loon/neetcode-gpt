783162d12d1fb2144c580dcf66378cec22e93b0d
H-Loon <119450081+H-Loon@users.noreply.github.com>
2026-05-07 14:50:58 +0300

    Update Backpropagation (Build a Neural Net 2/5)

--- foundations/backprop.py
+++ foundations/backprop.py
@@ -4,7 +4,9 @@ from typing import Tuple
 
 
 class Solution:
-    def backward(self, x: NDArray[np.float64], w: NDArray[np.float64], b: float, y_true: float) -> Tuple[NDArray[np.float64], float]:
+    def backward(
+        self, x: NDArray[np.float64], w: NDArray[np.float64], b: float, y_true: float
+    ) -> Tuple[NDArray[np.float64], float]:
         # x: 1D input array
         # w: 1D weight array
         # b: scalar bias
@@ -15,6 +17,8 @@ class Solution:
         # Return: (dL_dw rounded to 5 decimals, dL_db rounded to 5 decimals)
         z = x @ w + b
         y_hat = 1 / (1 + np.exp(-z))
-        dL_dw =(y_hat - y_true)*y_hat*(1-y_hat)*x
-        dL_db =(y_hat - y_true)*y_hat*(1-y_hat)
-        return (np.round(dL_dw,5),np.round(dL_db,5))
+
+        dL_db = (y_hat - y_true) * y_hat * (1 - y_hat)
+        dL_dw = dL_db * x
+
+        return (np.round(dL_dw, 5), round(dL_db, 5))
